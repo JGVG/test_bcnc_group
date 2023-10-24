bd79ecbe8af7d300ad501e5c7a98c6c04acf5b3c
José Gabriel Vázquez Gil <jgaby1997@gmail.com>
2023-10-24 18:05:03 +0200

    Successful get photos filtering by albums

--- src/main/kotlin/com/jgvg/test_bcnc_group/controllers/AlbumController.kt
+++ src/main/kotlin/com/jgvg/test_bcnc_group/controllers/AlbumController.kt
@@ -1,5 +1,6 @@
 package com.jgvg.test_bcnc_group.controllers
 import com.jgvg.test_bcnc_group.models.Album
+import com.jgvg.test_bcnc_group.models.Photo
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.client.RestTemplate
@@ -12,12 +13,22 @@ class AlbumController {
     val restTemplate = RestTemplate()
 
     @GetMapping
-    fun getAlbums(): List<Album> {
+    fun getAlbums(): List<Album> { // Ej: http://localhost:8080/albums
         val albumsList = restTemplate.getForObject(
                 "https://jsonplaceholder.typicode.com/albums",
                 Array<Album>::class.java
         )
 
         return albumsList?.toList() ?: emptyList()
+    }
+
+    @GetMapping("/{id}/photos")
+    fun getPhotoByAlbum(@PathVariable id: Int): List<Photo> { // Ej: http://localhost:8080/albums/2/photos
+        val photosList = restTemplate.getForObject(
+                "https://jsonplaceholder.typicode.com/photos?albumId=$id",
+                Array<Photo>::class.java
+        )
+
+        return photosList?.toList() ?: emptyList()
     }
 }
---
+++ src/main/kotlin/com/jgvg/test_bcnc_group/models/Photo.kt
@@ -0,0 +1,9 @@
+package com.jgvg.test_bcnc_group.models
+
+data class Photo(
+    val albumId: Int,
+    val id: Int,
+    val title: String,
+    val url: String,
+    val thumbnailUrl: String
+)
