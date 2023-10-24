1b95e4e8455de3ba064a1874f3dfb967a5ec7f38
José Gabriel Vázquez Gil <jgaby1997@gmail.com>
2023-10-24 16:32:15 +0200

    Init commit

---
+++ src/main/kotlin/com/jgvg/test_bcnc_group/TestBcncGroupApplication.kt
@@ -0,0 +1,11 @@
+package com.jgvg.test_bcnc_group
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestBcncGroupApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestBcncGroupApplication>(*args)
+}
---
+++ src/test/kotlin/com/jgvg/test_bcnc_group/TestBcncGroupApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jgvg.test_bcnc_group
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestBcncGroupApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
